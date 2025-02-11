9ddcac19652a58b9aa31698c88545132b27c498c
dylan-bloch <dylan.bloch@vikings.berry.edu>
2025-02-11 00:49:36 -0500

    started working somehow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LandmarkDiner.github.io/', // Replace with your actual GitHub repo name
+  base: '/LandmarkDiner/', // Replace with your actual GitHub repo name
   build: {
     outDir: 'dist',
 }});
